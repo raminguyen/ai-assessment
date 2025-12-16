753c41b2d686df5598934a33d9ade51ca1779db8
Rami Huu Nguyen <rami.nguyen12@gmail.com>
2025-12-16 12:27:55 -0500

    refactor to runner class

--- src/ai_assessment/model/model.py
+++ src/ai_assessment/model/model.py
@@ -1,5 +1,8 @@
 import time
 from datetime import datetime
+from ai_assessment import Rubric, ModelChatGPT, ModelClaude, ModelGemini3ProPreview, ModelGrok, Util
+import argparse
+
 
 class Model:
     def __init__(self, model_name):
@@ -76,3 +79,67 @@ class Model:
             "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     }
         return data
+    
+class Runner:
+    def __init__(self):
+        self.all_models = {
+            "gemini": ModelGemini3ProPreview(),
+            "chatgpt": ModelChatGPT(),
+            "claude": ModelClaude(),
+            "grok": ModelGrok()
+        }
+    
+    def generate(self, args):
+        model = self.all_models[args.model]
+        rubric = args.folder
+
+        if Util.check_data_exists(args.assignment, 'generate', args.model, rubric=rubric):
+            print('Already exists, skipping: ' + args.model + ' generate assignment ' + str(args.assignment))
+            return
+
+        essay = Util.create_essay(args.assignment)
+        data = model.generate(essay)
+        Util.save_individual_file(data, args.assignment, 'generate', args.model, rubric=rubric)
+
+    def tune(self, args):
+        model = self.all_models[args.model]
+
+        if Util.check_data_exists(args.assignment, 'tune', args.model, rubric=args.rubric):
+            print('Already exists, skipping: ' + args.model + ' tune assignment ' + str(args.assignment))
+            return
+
+        essay = Util.create_essay(args.assignment)
+        rubric = Rubric(args.rubric)
+        
+        data = model.tune(essay, rubric)
+        data['rubric'] = args.rubric
+        Util.save_individual_file(data, args.assignment, 'tune', args.model, rubric=args.rubric)
+
+    def score(self, args):
+        grader_model = self.all_models[args.grader]
+        
+        essay_text, writer_name, essay_type = Util.load_essay_from_data(args.filename, rubric=args.rubric)
+
+        if Util.check_data_exists(args.assignment, 'score', args.grader, essay_type=essay_type, writer=writer_name, rubric=args.rubric):
+            print('Already exists, skipping: ' + args.grader + ' scoring ' + essay_type + ' assignment ' + str(args.assignment))
+            return
+        
+        essay = Util.create_essay(args.assignment)
+        rubric = Rubric(args.rubric)
+        essay.essay_text = essay_text
+
+        data = grader_model.score(essay, rubric, writer=writer_name, essay_type=essay_type)
+        data['rubric'] = args.rubric
+        data['scored_essay_text'] = essay_text
+        data['essay_type'] = essay_type
+
+        Util.save_individual_file(data, args.assignment, 'score', args.grader, essay_type=essay_type, rubric=args.rubric, writer=writer_name)
+
+    def run(self, args):
+        """Route to command"""
+        if args.command == 'generate':
+            self.generate(args)
+        elif args.command == 'tune':
+            self.tune(args)
+        elif args.command == 'score':
+            self.score(args)
--- src/main.py
+++ src/main.py
@@ -1,15 +1,9 @@
 import argparse
 from datetime import datetime
-from ai_assessment import Rubric, ModelChatGPT, ModelClaude, ModelGemini3ProPreview, ModelGrok, Util
 import os 
-def main():
+from ai_assessment import Runner
 
-    all_models = {
-        "gemini": ModelGemini3ProPreview(),
-        "chatgpt": ModelChatGPT(),
-        "claude": ModelClaude(),
-        "grok": ModelGrok()
-    }
+def main():
 
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(dest='command')
@@ -32,80 +26,11 @@ def main():
     
     
     args = parser.parse_args()
+    Runner = Runner()
 
-    def generate_cmd(args):
-
-        model = all_models[args.model]
-        rubric = args.folder
-
-        if Util.check_data_exists(args.assignment, 'generate', args.model, rubric=rubric):
-            print('Already exists, skipping: ' + args.model + ' generate assignment ' + str(args.assignment))
-            return
-
-        model = all_models[args.model]
-
-        essay = Util.create_essay(args.assignment)
-
-        data = model.generate(essay)
-        
-        Util.save_individual_file(data, args.assignment, 'generate', args.model, rubric=rubric)
-
-
-    def tune_cmd(args):
-
-        model = all_models[args.model]
-
-        if Util.check_data_exists(args.assignment, 'tune', args.model, rubric=args.rubric):
-            print('Already exists, skipping: ' + args.model + ' tune assignment ' + str(args.assignment))
-            return
-
-        #1. Set up model, essay, and rubric
-        model = all_models[args.model]
-        essay = Util.create_essay(args.assignment)
-        rubric = Rubric(args.rubric)
-        
-        #2. Tune and save
-        data = model.tune(essay, rubric)
-        data['rubric'] = args.rubric
-        
-        Util.save_individual_file(data, args.assignment, 'tune', args.model, rubric=args.rubric)
-
-
-    def score_cmd(args):
-
-        grader_model = all_models[args.grader]
-
-        essay_type = 'generate' if 'gen' in args.filename else 'tune'
-        
-        # Load essay first to get writer_name
-        essay_text, writer_name, essay_type = Util.load_essay_from_data(args.filename, rubric=args.rubric)
-
-        if Util.check_data_exists(args.assignment, 'score', args.grader, essay_type=essay_type, writer=writer_name, rubric=args.rubric):
-            print('Already exists, skipping: ' + args.grader + ' scoring ' + essay_type + ' assignment ' + str(args.assignment))
-            return
-        
-        essay = Util.create_essay(args.assignment)
-        rubric = Rubric(args.rubric)
-        essay.essay_text = essay_text
-
-        data = grader_model.score(essay, rubric, writer=writer_name, essay_type=essay_type)
-        data['rubric'] = args.rubric
-        data['scored_essay_text'] = essay_text
-        data['essay_type'] = essay_type
-
-        Util.save_individual_file(data, args.assignment, 'score', args.grader, essay_type=essay_type, rubric=args.rubric, writer=writer_name)
-    
-    #
-    # Route to command
-    #
-
-    if args.command == 'generate':
-        generate_cmd(args)
-    elif args.command == 'tune':
-        tune_cmd(args)
-    elif args.command == 'score':
-        score_cmd(args)
-
+    Runner.generate(args)
+    Runner.tune(args)
+    Runner.score(args) 
 
 if __name__ == "__main__":
     main()
