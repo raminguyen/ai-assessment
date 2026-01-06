00caf7da4c00298bdeb8eeeac2fd27bbe5953eea
Rami Huu Nguyen <rami.nguyen12@gmail.com>
2026-01-06 17:21:33 -0500

    add new data

--- src/run_all.sh
+++ src/run_all.sh
@@ -16,10 +16,4 @@ bash run_pipeline.sh gemini critical_thinking 3
 bash run_pipeline.sh grok critical_thinking 3
 bash run_pipeline.sh claude critical_thinking 3
 
-#Assignment 2, Rubric: oral_communication
-bash run_pipeline.sh chatgpt oral_communication 1
-bash run_pipeline.sh gemini oral_communication 1
-bash run_pipeline.sh grok oral_communication 1
-bash run_pipeline.sh claude oral_communication 1
-
 echo "All done!"
