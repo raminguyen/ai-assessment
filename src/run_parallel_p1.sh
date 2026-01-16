70584a87df414d8496f154eb78b24991b05ca446
Rami Huu Nguyen <rami.nguyen12@gmail.com>
2026-01-16 08:45:05 -0500

    update a1 prompt 1 data

--- src/run_parallel_p1.sh
+++ src/run_parallel_p1.sh
@@ -4,9 +4,9 @@ echo "Starting all runs in parallel for Prompt 1 (p1)"
 
 # Assignment 1
 bash run_pipeline.sh chatgpt critical_thinking a1 p1 &
-#bash run_pipeline.sh gemini critical_thinking a1 p1 &
-#bash run_pipeline.sh grok critical_thinking a1 p1 &
-#bash run_pipeline.sh claude critical_thinking a1 p1 &
+bash run_pipeline.sh gemini critical_thinking a1 p1 &
+bash run_pipeline.sh grok critical_thinking a1 p1 &
+bash run_pipeline.sh claude critical_thinking a1 p1 &
 
 # Assignment 2
 # bash run_pipeline.sh chatgpt critical_thinking a2 p1 &
