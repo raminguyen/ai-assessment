8d5f892bff80d86328f4b54850a93819338479cd
Rami Huu Nguyen <rami.nguyen12@gmail.com>
2026-01-16 08:52:16 -0500

    update all data for a2, a3 , and p1

--- src/run_parallel_p1.sh
+++ src/run_parallel_p1.sh
@@ -9,16 +9,16 @@ bash run_pipeline.sh grok critical_thinking a1 p1 &
 bash run_pipeline.sh claude critical_thinking a1 p1 &
 
 # Assignment 2
-# bash run_pipeline.sh chatgpt critical_thinking a2 p1 &
-# bash run_pipeline.sh gemini critical_thinking a2 p1 &
-# bash run_pipeline.sh grok critical_thinking a2 p1 &
-# bash run_pipeline.sh claude critical_thinking a2 p1 &
+bash run_pipeline.sh chatgpt critical_thinking a2 p1 &
+bash run_pipeline.sh gemini critical_thinking a2 p1 &
+bash run_pipeline.sh grok critical_thinking a2 p1 &
+bash run_pipeline.sh claude critical_thinking a2 p1 &
 
 # Assignment 3
-# bash run_pipeline.sh chatgpt critical_thinking a3 p1 &
-# bash run_pipeline.sh gemini critical_thinking a3 p1 &
-# bash run_pipeline.sh grok critical_thinking a3 p1 &
-# bash run_pipeline.sh claude critical_thinking a3 p1 &
+bash run_pipeline.sh chatgpt critical_thinking a3 p1 &
+bash run_pipeline.sh gemini critical_thinking a3 p1 &
+bash run_pipeline.sh grok critical_thinking a3 p1 &
+bash run_pipeline.sh claude critical_thinking a3 p1 &
 
 # Wait for all background jobs to finish
 wait
