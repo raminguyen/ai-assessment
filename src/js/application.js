5ac492d2f21472a6440ee82f155c3ebe0d7eb837
Rami Huu Nguyen <rami.nguyen12@gmail.com>
2026-02-16 13:44:44 -0500

    update index0.html

--- src/js/application.js
+++ src/js/application.js
@@ -34,7 +34,7 @@ function copyText(text) {
 
 async function initApp() {
     // Use PROMPT_NUMBER if set (for index2.html), otherwise default to '1'
-    var promptNum = window.PROMPT_NUMBER || '1';
+    var promptNum = (window.PROMPT_NUMBER !== undefined) ? window.PROMPT_NUMBER : '1';
     await initRenderer(promptNum);
     document.getElementById('githubBtn').addEventListener('click', handleLoadFromGitHub);
     //document.getElementById('localBtn').addEventListener('click', handleLoadFromLocal);
